2ad3697a5f441fbea9432a404a035533eb4a8c64
Jason Snider <root@jasonsnider.com>
2019-02-11 11:36:54 -0600

    Fixed an auto-complete issue

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './user';
-import { lchmodSync } from 'fs';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json'})
