bac2493a0ae5fa2d4792da06c678b5a5d9c5ac0a
Jason Snider <root@jasonsnider.com>
2019-02-11 11:25:19 -0600

    Implement the auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { lchmodSync } from 'fs';
 
-@Injectable({
-  providedIn: 'root'
-})
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+};
+
+@Injectable()
 export class AuthService {
 
-  constructor() { }
+  //Set up the URL
+
+  private url: string;
+
+  
+  constructor(private http: HttpClient) {
+    let l = window.location;
+    this.url = `${l.protocol}://${l.hostname}/api/auth/`;
+  }
+
+  register(user: User): Observable<User>{
+    return this.http.post<User>(this.url + 'register', user, httpOptions);
+  }
+
+  logIn(user: User): Observable<User>{
+    return this.http.post<User>(this.url + 'login', user, httpOptions);
+  }
+
+  logOut(user: User): Observable<User>{
+    return this.http.get<User>(this.url + 'logout');
+  }
 }
