0141b238cea2ac7eda689c4d001454afce826a37
Jason Snider <root@jasonsnider.com>
2019-02-11 15:14:05 -0600

    Implented a basic login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+
 import { AuthService } from '../auth.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,37 @@ import { AuthService } from '../auth.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private authService: AuthService) {}
+  user:User = new User();
+  errors: Array<any> = [];
+  errorMessage: string;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {}
+
+  response(response): void{
+    if(response.success===false){
+      this.errors = response.error.errors;
+      this.errorMessage = response.error.message;
+    }
+
+    if(response.success===true){
+      
+    }
+  }
+
+  onSubmit(): void{
+    
+console.log(this.user);
+
+    this.authService.logIn(this.user).subscribe(
+      (response) => {
+        //this.response(response);
+        console.log(response);
+      }
+    );
   }
 
 }
