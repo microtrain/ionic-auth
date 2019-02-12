dede164f5101fc5636d6dd7bd412d9614a1ae7af
Jason Snider <root@jasonsnider.com>
2019-02-12 11:10:31 -0600

    Completed login logic - Lab 1 and 2

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,7 +12,6 @@ import { User } from '../user';
 export class LoginPage implements OnInit {
 
   user:User = new User();
-  errors: Array<any> = [];
   errorMessage: string;
 
   constructor(
@@ -23,23 +22,18 @@ export class LoginPage implements OnInit {
 
   response(response): void{
     if(response.success===false){
-      this.errors = response.error.errors;
-      this.errorMessage = response.error.message;
+      this.errorMessage = 'Invalid Credentials';
     }
 
     if(response.success===true){
-      
+      window.location.href = '/';
     }
   }
 
   onSubmit(): void{
-    
-console.log(this.user);
-
     this.authService.logIn(this.user).subscribe(
       (response) => {
-        //this.response(response);
-        console.log(response);
+        this.response(response);
       }
     );
   }
